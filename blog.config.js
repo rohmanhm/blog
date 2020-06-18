2b16341de9a146b71b1ae4ea0f1c265cdd762c59
Rohman Habib M <mhrohman@live.com>
2020-06-18 12:35:27 +0700

    feat: initial commit

---
+++ blog.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMeta: {
+    title: "Rohman HM Blog",
+    author: "Rohman Habib M",
+    image: "/public/site-feature.png",
+    description: "Rohman HM blog",
+    siteUrl: "https://roh.app/",
+    social: {
+      twitter: "rohmanhm",
+    },
+    postsPerPage: 5,
+  },
+};
---
+++ posts/rss-feed.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+const posts = require("./get-blog-posts");
+const { siteMeta } = require("../blog.config");
+
+// https://jsonfeed.org/version/1
+const feed = {
+  version: "https://jsonfeed.org/version/1",
+  title: siteMeta.title,
+  home_page_url: siteMeta.siteUrl,
+  feed_url: `${siteMeta.siteUrl}/feed.json`,
+  description: siteMeta.description,
+  icon: `${siteMeta.siteUrl}/public/apple-touch-icon-152x152.png`,
+  favicon: `${siteMeta.siteUrl}/public/favicon.ico`,
+  author: {
+    name: siteMeta.author,
+    url: siteMeta.siteUrl,
+    avatar: `${siteMeta.siteUrl}/rohmanhm.png`,
+  },
+  items: posts.map((post) => ({
+    id: `${siteMeta.siteUrl}${post.path}`,
+    url: `${siteMeta.siteUrl}${post.path}`,
+    title: post.title,
+    content_text: `${post.summary} - ${siteMeta.siteUrl}${post.path}`,
+    summary: post.summary,
+    image: `${siteMeta.siteUrl}${post.image}`,
+    date_published: post.publishedAt,
+    author: siteMeta.author,
+  })),
+};
+
+fs.writeFileSync(
+  path.join("./.next/static", "feed.json"),
+  JSON.stringify(feed)
+);
